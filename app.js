685f1c4839f3f7f2c523253b42fb5c9990219b7f
shxnying <62127289+shxnying@users.noreply.github.com>
2022-09-02 22:38:10 +0800

    consolidate posts

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+var posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -31,6 +32,21 @@ app.get("/contact", function(req,res){
   res.render("contact", {
     contentForContactPage: contactContent
   });
+});
+
+app.get("/compose", function(req,res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req,res){
+  const composedpost = {
+    postTitle : req.body.titleText,
+    postContents: req.body.postBody
+  }
+
+  posts.push(composedpost);
+  console.log(posts);
+  res.redirect("/");
 });
 
 
